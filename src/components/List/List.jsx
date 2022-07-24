ae92bd207458bd253c7d5e70477bc57d7a0b1391
Fakevin0613 <ke_qinyang@outlook.com>
2022-07-24 14:28:36 -0400

    adding select menu

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core'
+import { useState } from 'react';
+import useStyle from './styles';
 
 const List = () => {
-    return(
-        <h1>List</h1>
-    );
+    const classes = useStyle();
+    const [type, setType] = useState('restaurant');
+    const [rating, setRating] = useState('0');
+
+    return (
+        <div className={classes.container}>
+            <Typography variant="h5">
+                Where are you heading to?
+            </Typography>
+            <FormControl className={classes.formControl}>
+                <InputLabel>What you are looking for?</InputLabel>
+                <Select value={type} onChange={(e) => setType(e.target.value)}>
+                    <MenuItem value="restaurant">Restaurant</MenuItem>
+                    <MenuItem value="Hotels">Hotels</MenuItem>
+                    <MenuItem value="Attractions">Attractions</MenuItem>
+                </Select>
+            </FormControl>
+
+            <FormControl className={classes.formControl}>
+                <InputLabel>Rating</InputLabel>
+                <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+                    <MenuItem value="0">All</MenuItem>
+                    <MenuItem value="2">Rating above 2.0</MenuItem>
+                    <MenuItem value="3">Rating above 3.0</MenuItem>
+                    <MenuItem value="4">Rating above 4.0</MenuItem>
+                    <MenuItem value="4.5">Rating above 4.5</MenuItem>
+                </Select>
+            </FormControl>
+        </div>
+
+    )
 }
 
 export default List;
